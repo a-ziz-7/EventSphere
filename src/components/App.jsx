3ffdb4f6c5cefbfbd5ac1d68260e2562e238e215
Carlo Ace Sagad <carloace166@gmail.com>
2024-09-18 23:39:02 -0400

    Fixed our vite react app to be much cleaner and easier to understand. Removed unnecessary files.

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDom.render(<App />, document.getElementById("root"));
