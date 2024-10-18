0d5a199164d126181dd374cd67dd59e39938fc91
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2024-10-17 21:40:28 -0400

    Signup page adjusted to databse standard.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,114 +6,132 @@ function Signup() {
     <>
       <Navbar />
       <div className="flex min-h-screen flex-col justify-center items-center py-8">
-        <h1 className="text-4xl font-bold mb-8 text-center">
+        <h1 className="text-4xl font-bold pt-20 mb-8 text-center">
           Registration Form
         </h1>
-        <form className="w-full max-w-lg mx-auto bg-white p-12 rounded-md shadow-md">
-          {/* First Name */}
+        {/* Increased width of the form container */}
+        <form className="w-full max-w-2xl mx-auto bg-white p-12 rounded-md shadow-md">
+          {/* First Name and Last Name in the same row */}
+          <div className="flex justify-between mb-6">
+            <div className="w-1/2 mr-2">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="first-name"
+              >
+                First Name
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="text"
+                id="first-name"
+                name="firstName"
+                placeholder="First Name"
+              />
+            </div>
+            <div className="w-1/2 ml-2">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="last-name"
+              >
+                Last Name (Optional)
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="text"
+                id="last-name"
+                name="lastName"
+                placeholder="Last Name"
+              />
+            </div>
+          </div>
+
+          {/* Username */}
           <div className="mb-6">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="first-name"
+              htmlFor="username"
             >
-              First Name
+              Username
             </label>
             <input
-              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
               type="text"
-              id="first-name"
-              name="firstName"
-              placeholder="First Name"
+              id="username"
+              name="username"
+              placeholder="Username"
             />
           </div>
 
-          {/* Last Name */}
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="last-name"
-            >
-              Last Name
-            </label>
-            <input
-              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              type="text"
-              id="last-name"
-              name="lastName"
-              placeholder="Last Name"
-            />
+          {/* Email and Phone in the same row */}
+          <div className="flex justify-between mb-6">
+            <div className="w-1/2 mr-2">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email@example.com"
+              />
+            </div>
+            <div className="w-1/2 ml-2">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="phone"
+              >
+                Phone Number (Optional)
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="tel"
+                id="phone"
+                name="phone"
+                placeholder="(123) 456-7890"
+              />
+            </div>
           </div>
 
-          {/* Email */}
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Email@example.com"
-            />
-          </div>
-
-          {/* Phone Number */}
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="phone"
-            >
-              Phone Number
-            </label>
-            <input
-              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              type="tel"
-              id="phone"
-              name="phone"
-              placeholder="(123) 456-7890"
-            />
-          </div>
-
-          {/* Password */}
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="********"
-            />
-          </div>
-
-          {/* Confirm Password */}
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="confirm-password"
-            >
-              Confirm Password
-            </label>
-            <input
-              className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              type="password"
-              id="confirm-password"
-              name="confirmPassword"
-              placeholder="********"
-            />
+          {/* Password and Confirm Password in the same row */}
+          <div className="flex justify-between mb-6">
+            <div className="w-1/2 mr-2">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="********"
+              />
+            </div>
+            <div className="w-1/2 ml-2">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="confirm-password"
+              >
+                Confirm Password
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="password"
+                id="confirm-password"
+                name="confirmPassword"
+                placeholder="********"
+              />
+            </div>
           </div>
 
           <button
-            className="w-full bg-indigo-500 text-white text-lg font-bold py-3 rounded-md hover:bg-indigo-600 transition duration-300"
+            className="w-full bg-indigo-500 text-white text-lg font-bold py-2 rounded-md hover:bg-indigo-600 transition duration-300"
             type="submit"
           >
             Register
