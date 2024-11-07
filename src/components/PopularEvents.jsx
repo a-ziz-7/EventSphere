5b23fcfc195bcb6dda467b0e608bb09c71c8bf24
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2024-11-07 16:00:22 -0500

    Brovse events page bug fixed.

--- src/components/PopularEvents.jsx
+++ src/components/PopularEvents.jsx
@@ -4,11 +4,9 @@ import EventsList from "./EventsList";
 import Navbar from "./Navbar";
 
 function PopularEvents() {
-  const [events, setEvents] = useState([]);
   const [filteredEvents, setFilteredEvents] = useState([]);
   const [userLocation, setUserLocation] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [showingNearby, setShowingNearby] = useState(true);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -17,27 +15,22 @@ function PopularEvents() {
           lat: position.coords.latitude,
           lon: position.coords.longitude,
         });
-      },
-      (error) => {
-        console.error("Error getting user location: ", error);
-        setUserLocation({ lat: 40.7128, lon: -74.006 });
       }
     );
-    console.log(userLocation);
+    // console.log(userLocation);
   }, []);
 
   useEffect(() => {
     console.log(userLocation);
     if (userLocation) {
-      // alert(userLocation.lat +"   "+userLocation.lon);
       const fetchData = async () => {
         try {
           const response = await axios.get(
             `http://localhost:5000/api/events?location=${userLocation.lon},${userLocation.lat}`
           );
           console.log("Fetched events:", response.data.length);
-          setEvents(response.data);
-          filterEvents(response.data);
+          const sortedEvents = response.data.sort((a, b) => new Date(a.time) - new Date(b.time));
+          setFilteredEvents(sortedEvents.slice(0, 28));
         } catch (error) {
           console.log("Error fetching events: ", error);
         } finally {
@@ -48,62 +41,6 @@ function PopularEvents() {
     }
   }, [userLocation]);
 
-  const filterEvents = (allEvents) => {
-    if (!userLocation) return;
-    const maxDistance = 50;
-
-    const today = new Date();
-    const tomorrow = new Date(today);
-    tomorrow.setDate(today.getDate() + 1);
-
-    const nearbyEvents = allEvents.filter((event) => {
-      const eventDate = new Date(event.time);
-      const isInDateRange = eventDate >= today && eventDate <= tomorrow;
-      // console.log(event)
-      // console.log(event.location);
-
-      const eventLocation = { lat: event.latitude, lon: event.longitude };
-
-      const isNearLocation =
-        calculateDistance(
-          userLocation.lat,
-          userLocation.lon,
-          eventLocation.lat,
-          eventLocation.lon
-        ) <= maxDistance;
-
-      return isInDateRange && isNearLocation;
-    });
-
-    if (nearbyEvents.length === 0) {
-      console.log("No nearby events found. Showing other events.");
-      setShowingNearby(false);
-      const otherEvents = allEvents.slice(0, 16);
-      setFilteredEvents(otherEvents);
-    } else {
-      console.log("Nearby events found.");
-      setShowingNearby(true);
-      setFilteredEvents(nearbyEvents.slice(0, 16));
-    }
-  };
-
-  const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    const R = 6371;
-    const dLat = toRad(lat2 - lat1);
-    const dLon = toRad(lon2 - lon1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(lat1)) *
-        Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c;
-  };
-
-  const toRad = (value) => {
-    return (value * Math.PI) / 180;
-  };
 
   if (loading) {
     return (
