05d95c3b0dc8cb31971fcea287b0d8c9b8603a95
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2024-12-15 01:43:28 -0500

    Changes 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure Vite outputs to 'dist' directory
+    // If you have assets like images or fonts, you might want to add:
+    assetsDir: 'assets',
+  }
 })
