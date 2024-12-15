8c4c649ad1048563c393fa866b203200ca3e7769
a-ziz-7 <aziz.abdusamiev7@gmail.com>
2024-12-15 12:28:28 -0500

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure Vite outputs to 'dist' directory
-    // If you have assets like images or fonts, you might want to add:
-    assetsDir: 'assets',
-  }
-})
-
+    outDir: "dist", // Ensure the output directory is correct
+  },
+});
